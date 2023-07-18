aafa65896c646b2440cba014ef86fadd23f769fb
HBA114 <hbasriayhaner114@gmail.com>
2023-07-18 18:27:33 +0300

    Added rotations to wheel shapes

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -18,11 +18,32 @@ public class CarController : MonoBehaviour
             {
                 axleInfo.leftWheel.steerAngle = steering;
                 axleInfo.rightWheel.steerAngle = steering;
+                axleInfo.leftWheelShape.transform.localRotation =
+                    Quaternion.Euler(
+                        new Vector3(motor,
+                            steering - 180,
+                            axleInfo.leftWheelShape.transform.localRotation.z));
+                axleInfo.rightWheelShape.transform.localRotation =
+                    Quaternion.Euler(
+                        new Vector3(-motor,
+                            steering,
+                            axleInfo.rightWheelShape.transform.localRotation.z));
             }
+
             if (axleInfo.motor)
             {
                 axleInfo.leftWheel.motorTorque = motor;
                 axleInfo.rightWheel.motorTorque = motor;
+                axleInfo.leftWheelShape.transform.localRotation =
+                    Quaternion.Euler(
+                        new Vector3(motor,
+                            axleInfo.leftWheelShape.transform.localRotation.y - 180,
+                            axleInfo.leftWheelShape.transform.localRotation.z));
+                axleInfo.rightWheelShape.transform.localRotation =
+                    Quaternion.Euler(
+                        new Vector3(-motor,
+                            axleInfo.rightWheelShape.transform.localRotation.y,
+                            axleInfo.rightWheelShape.transform.localRotation.z));
             }
         }
     }
@@ -33,6 +54,8 @@ public class AxleInfo
 {
     public WheelCollider leftWheel;
     public WheelCollider rightWheel;
+    public GameObject leftWheelShape;
+    public GameObject rightWheelShape;
     public bool motor;
     public bool steering;
 }
