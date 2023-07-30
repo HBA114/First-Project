85d2bcb1e06d7cfbb3da5b87040b96120981a8d0
Hasan Basri Ayhaner <hasan.ayhaner@nextlua.com>
2023-07-30 17:01:08 +0300

    Updated CarController

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,49 +1,82 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
+    private Rigidbody _rigidbody;
     public List<AxleInfo> axleInfos;
     public float maxMotorTorque;
     public float maxSteeringAngle;
+    private float _speed;
+
+    void Start()
+    {
+        _rigidbody = gameObject.GetComponent<Rigidbody>();
+    }
 
     void FixedUpdate()
     {
         float motor = maxMotorTorque * Input.GetAxis("Vertical");
         float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+        _speed = _rigidbody.velocity.magnitude;
+        float wheelSpeed = (float)(_speed / (2 * Math.PI * axleInfos[0].leftWheel.radius));
+
+        Debug.Log("Speed (m/s): " + _speed);
+        Debug.Log("Wheel Speed (rps): " + wheelSpeed);
 
         foreach (AxleInfo axleInfo in axleInfos)
         {
+            axleInfo.leftWheelShape.transform.localRotation =
+                Quaternion.Euler(
+                    new Vector3(wheelSpeed * 360,
+                        axleInfo.leftWheelShape.transform.localRotation.y - 180,
+                        axleInfo.leftWheelShape.transform.localRotation.z));
+            axleInfo.rightWheelShape.transform.localRotation =
+                Quaternion.Euler(
+                    new Vector3(-wheelSpeed * 360,
+                        axleInfo.rightWheelShape.transform.localRotation.y,
+                        axleInfo.rightWheelShape.transform.localRotation.z));
             if (axleInfo.steering)
             {
                 axleInfo.leftWheel.steerAngle = steering;
                 axleInfo.rightWheel.steerAngle = steering;
+                
                 axleInfo.leftWheelShape.transform.localRotation =
                     Quaternion.Euler(
-                        new Vector3(motor,
+                        new Vector3(axleInfo.leftWheelShape.transform.localRotation.x,
                             steering - 180,
                             axleInfo.leftWheelShape.transform.localRotation.z));
                 axleInfo.rightWheelShape.transform.localRotation =
                     Quaternion.Euler(
-                        new Vector3(-motor,
+                        new Vector3(axleInfo.rightWheelShape.transform.localRotation.x,
                             steering,
                             axleInfo.rightWheelShape.transform.localRotation.z));
             }
 
             if (axleInfo.motor)
             {
-                axleInfo.leftWheel.motorTorque = motor;
-                axleInfo.rightWheel.motorTorque = motor;
-                axleInfo.leftWheelShape.transform.localRotation =
-                    Quaternion.Euler(
-                        new Vector3(motor,
-                            axleInfo.leftWheelShape.transform.localRotation.y - 180,
-                            axleInfo.leftWheelShape.transform.localRotation.z));
-                axleInfo.rightWheelShape.transform.localRotation =
-                    Quaternion.Euler(
-                        new Vector3(-motor,
-                            axleInfo.rightWheelShape.transform.localRotation.y,
-                            axleInfo.rightWheelShape.transform.localRotation.z));
+                if (axleInfo.rightWheel.rpm < 5500)
+                {
+                    axleInfo.leftWheel.motorTorque = motor;
+                    axleInfo.rightWheel.motorTorque = motor;
+                }
+                else
+                {
+                    axleInfo.leftWheel.motorTorque -= 100;
+                    axleInfo.rightWheel.motorTorque -= 100;
+                }
+
+                // axleInfo.leftWheelShape.transform.localRotation =
+                //     Quaternion.Euler(
+                //         new Vector3((float)_speed * 100,
+                //             axleInfo.leftWheelShape.transform.localRotation.y - 180,
+                //             axleInfo.leftWheelShape.transform.localRotation.z));
+                // axleInfo.rightWheelShape.transform.localRotation =
+                //     Quaternion.Euler(
+                //         new Vector3((float)-_speed * 100,
+                //             axleInfo.rightWheelShape.transform.localRotation.y,
+                //             axleInfo.rightWheelShape.transform.localRotation.z));
             }
         }
     }
